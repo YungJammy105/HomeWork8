d722d660d19f1414ef500523cb7b5ac8328e52f6
Ilya Andreev <Franklylakers@yandex.ru>
2023-01-05 17:48:14 +0700

    add Example56

---
+++ Example56/Program.cs
@@ -0,0 +1,34 @@
+﻿using MyMethod;
+
+MyGenerate gen = new ();
+
+int[,] myArr = gen.ArrayGenDI();
+MyMethod.MyMethod.Print(myArr);
+
+int maxString = MaxString(myArr);
+MyMethod.MyMethod.Print($"Минимальную сумму имеет {maxString} строка");
+
+int MaxString(int[,] usersArray)
+{
+    int length = usersArray.GetLength(0);
+    int maxStringInd = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (StringSum(usersArray, i) < StringSum(usersArray, maxStringInd))
+        {
+            maxStringInd = i;
+        }
+    }
+    return maxStringInd + 1;
+}
+
+int StringSum(int[,] usersArray, int stringIndex)
+{
+    int sum = 0;
+    for (int i = 0; i < usersArray.GetLength(1); i++)
+    {
+        sum += usersArray[stringIndex, i];
+    }
+    return sum;
+}
+
